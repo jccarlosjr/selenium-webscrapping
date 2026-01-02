3c98fc450a117469225dc810b26945a050c1bc1a
Carlos Jr <jc.carlos.junior@outlook.com>
2026-01-02 15:10:29 -0300

    RMC and RCC added

--- scrapper/selenium_driver.py
+++ scrapper/selenium_driver.py
@@ -24,6 +24,14 @@ def init_driver():
     options = webdriver.ChromeOptions()
     options.add_argument(LOCAL_USER_PATH)
 
+    options.add_argument("--no-first-run")
+    options.add_argument("--no-default-browser-check")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--remote-debugging-port=9222")
+
+
     _driver = webdriver.Chrome(
         service=chrome_service,
         options=options
